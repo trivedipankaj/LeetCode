1dad3ded49c3e47e87a9168ec53304df8d5a11f0
Pankaj Trivedi <pankajk@valyoo.in>
2015-03-14 01:05:20 +0530

    added checking the palindrome

---
+++ IsPalindrome.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public boolean isPalindrome(String s) {
+       int len = s.length();
+       if(len<=1){
+           return true;
+       }
+       int start=0,end=len-1;
+       while(start <end){
+           while(!Character.isLetterOrDigit(s.charAt(start)) && start <end)
+           { start++;}
+           while(!Character.isLetterOrDigit(s.charAt(end)) && end>0){
+               end--;
+           }
+           if(Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end)) && start <=end)
+           {
+               return false;
+           }else{
+               start++;end--;
+               flag=true;
+           }
+       }
+       return true;
+    }
+}
