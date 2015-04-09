9b2acb0a9fb1bf725f383e9986c34e121232bf0f
Pankaj Trivedi <pankajk@valyoo.in>
2015-04-10 00:31:11 +0530

    added linked list cycle-EASY

---
+++ LinkedListCycle.java
@@ -0,0 +1,38 @@
+/****
+Given a linked list, determine if it has a cycle in it.
+
+Follow up:
+Can you solve it without using extra space?
+*/
+
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow =head;
+        ListNode fast = head;
+        
+        if(head == null || head.next ==null){
+            return false;
+        }
+        
+        while(fast!=null && fast.next!=null){
+            slow =slow.next;
+            fast = fast.next.next;
+            if(slow ==fast){
+                return true;
+            }
+        }
+        return false;
+    }
+}
