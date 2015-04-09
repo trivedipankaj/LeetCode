56bf93ccc85920e2da283cf5426e29545dc24829
Pankaj Trivedi <pankajk@valyoo.in>
2015-04-10 00:47:21 +0530

    added linked list cycle2-EASY

---
+++ LinkedListCycle2.java
@@ -0,0 +1,43 @@
+/****
+Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+Follow up:
+Can you solve it without using extra space?
+*/
+
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow =head;
+        ListNode fast = head;
+    
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow ==fast){
+                break;
+            }
+        }
+        
+        if(fast == null || fast.next ==null){
+            return null;
+        }
+        slow=head;
+        while(slow!=fast){
+            slow =slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+}
